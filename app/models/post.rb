3cfe38370739e9cae560a4a081bf64a4194468ef
LondonBishop <londonbishop@codecutters.co.uk>
2019-03-18 17:30:53 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :check_clickbait
+
+
+  private
+
+  def check_clickbait
+
+      if (/Won't Believe|Secret|Top\d*|Guess/).match(title) == nil
+          errors.add(:title, "no nasty stuff please")
+      end
+    
+  end
+
 end
